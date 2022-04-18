ac50f2a2f0d0acd017c8ca7f0e0f85deb84d8476
Joel Guzman <joel-guzman@hotmail.com>
2022-04-18 15:29:48 -0500

    Ejercicio8, determina si un dia de semana es laboral

--- src/main/java/com/sofkau/taller/ejercicio8/Ejercicio8.java
+++ src/main/java/com/sofkau/taller/ejercicio8/Ejercicio8.java
@@ -1,21 +1,44 @@
 package com.sofkau.taller.ejercicio8;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
 
     public static void main(String[] args) {
         mostrarDias();
+        String diaUsuario = leerDia();
+        String mensaje = "";
+        switch (diaUsuario) {
+            case "lunes":
+            case "martes":
+            case "miercoles":
+            case "jueves":
+            case "viernes":
+                mensaje = diaUsuario.concat(" es un dia laboral");
+                break;
+            case "sabado":
+            case "domingo":
+                mensaje = diaUsuario.concat(" no es un dia laboral");
+                break;
+            default:
+                mensaje = diaUsuario.concat(" no es un dia de semana");
+        }
+
+        System.out.println(mensaje);
+
+    }
+
+    private static String leerDia() {
+        Scanner scanner = new Scanner(System.in);
+        String dia = scanner.next().toLowerCase();
+        scanner.close();
+        return dia;
     }
 
     public static void mostrarDias() {
         System.out.println(":: Dias de la semana ::");
-        System.out.println("Ingrese una opcion para saber si el dia es laboral: ");
-        System.out.println("1. Lunes");
-        System.out.println("2. Martes");
-        System.out.println("3. Miercoles");
-        System.out.println("4. Jueves");
-        System.out.println("5. Viernes");
-        System.out.println("6. Sabado");
-        System.out.println("7. Domingo");
+        System.out.println("Escriba un dia para determinar si es laboral: \n" +
+                "lunes, martes, miercoles, jueves, viernes, sabado y domingo");
     }
 
 }

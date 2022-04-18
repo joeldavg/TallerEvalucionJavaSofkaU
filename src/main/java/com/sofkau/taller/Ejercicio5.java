b291e8632e7ebda16c45090993aa5dbbd308a1d6
Joel Guzman <joel-guzman@hotmail.com>
2022-04-18 13:55:26 -0500

    Ejercicio5, numeros impares y pares desde 1 al 100, ciclo while

---
+++ src/main/java/com/sofkau/taller/Ejercicio5.java
@@ -0,0 +1,27 @@
+package com.sofkau.taller;
+
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 100) {
+            i++;
+            evaluarNumero(i);
+        }
+    }
+
+    private static void evaluarNumero(int i) {
+        String mensaje = i + ": es impar";
+        if (esPar(i)) {
+            mensaje = i + ": es par";
+        }
+        System.out.println(mensaje);
+    }
+
+    public static boolean esPar(int x) {
+        boolean isPar = x % 2 == 0;
+
+        return isPar;
+    }
+
+}

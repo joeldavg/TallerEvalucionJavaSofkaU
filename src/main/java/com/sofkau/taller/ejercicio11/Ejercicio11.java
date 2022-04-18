5976f01c7c4e67f5afc7d604b2a8ca1384cbd50e
Joel Guzman <joel-guzman@hotmail.com>
2022-04-18 17:36:35 -0500

    Ejercicio11, este ejercicio muestra la longitud de una frase y la cantidad de vocales

---
+++ src/main/java/com/sofkau/taller/ejercicio11/Ejercicio11.java
@@ -0,0 +1,53 @@
+package com.sofkau.taller.ejercicio11;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Ejercicio11 {
+
+    public static void main(String[] args) {
+
+        //Leer dato consola
+        System.out.println("Ingresar frase:");
+        String textoUsuario = leerTexto();
+
+        int textoLength = textoUsuario.length();
+        Map<Character, Integer> vocales = contarVocales(textoUsuario);
+
+        System.out.println("La longitud de la frase es: " + textoLength);
+        System.out.println("La frase contiene la siguiente cantidad de vocales: " + vocales);
+    }
+
+    private static String leerTexto() {
+        Scanner scanner = new Scanner(System.in);
+        String texto = scanner.nextLine();
+        scanner.close();
+        return texto;
+    }
+
+    private static Map<Character, Integer> contarVocales(String texto) {
+
+        Map<Character, Integer> vocales = new HashMap<>();
+        vocales.put('a', 0);
+        vocales.put('e', 0);
+        vocales.put('i', 0);
+        vocales.put('o', 0);
+        vocales.put('u', 0);
+
+        for (int i = 0; i < texto.length(); i++) {
+            Character letra = texto.charAt(i);
+            CompararLetra(vocales, letra);
+        }
+
+        return vocales;
+    }
+
+    private static void CompararLetra(Map<Character, Integer> vocales, Character letra) {
+        if (vocales.containsKey(letra)) {
+            int cantidad = vocales.get(letra);
+            vocales.put(letra, ++cantidad);
+        }
+    }
+
+}
